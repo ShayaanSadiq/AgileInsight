e1c1b42c86d9c48be83aaf9a19b3437fff5ab0aa
Jawadmohd <jawadmohd@2706@gmail.com>
2026-04-03 00:10:55 +0530

    worked on the manager's analytics frontend.

--- frontend/src/manager/components/ProjectPageHeader.jsx
+++ frontend/src/manager/components/ProjectPageHeader.jsx
@@ -2,25 +2,13 @@ import React from "react";
 import "../css/ProjectPageHeader.css";
 
 export const ProjectPageHeader = () => {
-  const analytics = {
-    tasksCompleted: 5,
-    totalTasks: 10,
-    percentageProjectCompleted: "50%",
-    deadLine: "2030",
-  };
   return (
-    <>
-      <section className="project-page-first-section">
-        <div className="project-page-box">
-          Tasks completed: {analytics.tasksCompleted}
-        </div>
-        <div className="project-page-box">
-          Project completed: {analytics.percentageProjectCompleted}
-        </div>
-        <div className="project-page-box">Deadline: {analytics.deadLine}</div>
-        <div className="project-page-box">what more?</div>
-        <div className="project-page-box">what more??</div>
-      </section>
-    </>
+    <section className="project-page-first-section">
+      <div className="project-page-header-options">
+        <button className="project-page-header-options-button">Project</button>
+        <button className="project-page-header-options-button">Sprint</button>
+        <button className="project-page-header-options-button">Task</button>
+      </div>
+    </section>
   );
 };
--- frontend/src/manager/components/ProjectPageMainDiv.jsx
+++ frontend/src/manager/components/ProjectPageMainDiv.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { ProjectPageHeader } from "./ProjectPageHeader";
+import { ProjectPageAnalyticsDiv } from "./ProjectPageAnalyticsDiv";
 import "../css/ProjectPageMainDiv.css";
 
 export const ProjectPageMainDiv = () => {
   return (
     <div className="project-page-main">
       <ProjectPageHeader />
-      hi
+      <ProjectPageAnalyticsDiv />
     </div>
   );
 };
