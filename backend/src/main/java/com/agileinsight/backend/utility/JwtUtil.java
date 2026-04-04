4c2559fe116e7b01ad75d76c130111e383430b1b
Shayaan Sadiq <shayaansadiq1402@gmail.com>
2026-04-04 15:13:39 +0530

    testing JwtUtil

--- backend/src/main/java/com/agileinsight/backend/utility/JwtUtil.java
+++ backend/src/main/java/com/agileinsight/backend/utility/JwtUtil.java
@@ -1,42 +1,42 @@
-package com.agileinsight.backend.utility;
+// package com.agileinsight.backend.utility;
 
-import java.security.Key;
-import java.util.Date;
+// import java.security.Key;
+// import java.util.Date;
 
-import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SignatureAlgorithm;
+// import io.jsonwebtoken.Jwts;
+// import io.jsonwebtoken.security.Keys;
+// import io.jsonwebtoken.security.SignatureAlgorithm;
 
-@Component
-public class JwtUtil {
+// @Component
+// public class JwtUtil {
 
-    private final String SECRET = "kuchbhisecretjwtkaaaaaaaaaaaaaaaaaaa";
+//     private final String SECRET = "kuchbhisecretjwtkaaaaaaaaaaaaaaaaaaa";
 
-    private Key getSignKey() {
-        return Keys.hmacShaKeyFor(SECRET.getBytes());
-    }
+//     private Key getSignKey() {
+//         return Keys.hmacShaKeyFor(SECRET.getBytes());
+//     }
 
-    public String generateToken(String username) {
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hr
-                .signWith(getSignKey(), SignatureAlgorithm.HS256)
-                .compact();
-    }
+//     public String generateToken(String username) {
+//         return Jwts.builder()
+//                 .setSubject(username)
+//                 .setIssuedAt(new Date())
+//                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hr
+//                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
+//                 .compact();
+//     }
 
-    public String extractUsername(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSignKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
+//     public String extractUsername(String token) {
+//         return Jwts.parserBuilder()
+//                 .setSigningKey(getSignKey())
+//                 .build()
+//                 .parseClaimsJws(token)
+//                 .getBody()
+//                 .getSubject();
+//     }
 
-    public boolean validateToken(String token, String username) {
-        return extractUsername(token).equals(username);
-    }
-}
+//     public boolean validateToken(String token, String username) {
+//         return extractUsername(token).equals(username);
+//     }
+// }
