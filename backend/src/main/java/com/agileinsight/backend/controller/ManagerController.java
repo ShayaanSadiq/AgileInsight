fe9021c36e3e6881cb2142d22705b4022d7cca19
Shayaan Sadiq <shayaansadiq1402@gmail.com>
2026-04-04 15:07:10 +0530

    CORS update

--- backend/src/main/java/com/agileinsight/backend/controller/ManagerController.java
+++ backend/src/main/java/com/agileinsight/backend/controller/ManagerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/managers")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ManagerController {
     
     @Autowired
--- backend/src/main/java/com/agileinsight/backend/controller/TaskController.java
+++ backend/src/main/java/com/agileinsight/backend/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.agileinsight.backend.controller;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/tasks")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TaskController {
     
     @Autowired
