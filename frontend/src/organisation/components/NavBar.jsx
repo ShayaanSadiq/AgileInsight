6f2bf0782f440377dc6d6714f6e04e8c7b5c898b
Jawadmohd <jawadmohd@2706@gmail.com>
2026-04-06 15:22:52 +0530

    workden on api

--- frontend/src/organisation/components/NavBar.jsx
+++ frontend/src/organisation/components/NavBar.jsx
@@ -5,10 +5,10 @@ export const NavBar = () => {
   return (
     <nav className="nav-bar">
       <h2>AgileInsight</h2>
-      <ul className="nav-list">
-        <li className="nav-list-li">Settings</li>
-        <li className="nav-list-li">Logout</li>
-      </ul>
+      <select name="\" id="">
+        <option value="">Settings</option>
+        <option value="">Create project</option>
+      </select>
     </nav>
   );
 };
