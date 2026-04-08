8d7f5c96d8c270fe9edd823fa5aef5e80e9a5e07
Shayaan Sadiq <shayaansadiq1402@gmail.com>
2026-04-09 03:35:57 +0530

    JWT expiry fix

--- backend/src/main/java/com/agileinsight/backend/utility/JwtUtil.java
+++ backend/src/main/java/com/agileinsight/backend/utility/JwtUtil.java
@@ -23,7 +23,7 @@ public class JwtUtil {
                 .subject(username)
                 .claim("id", id)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 30))
+                .expiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24))
                 .signWith(getSignKey())
                 .compact();
     }
