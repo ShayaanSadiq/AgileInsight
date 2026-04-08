c8bba14a3380a82ad4ee7c794a2cce16d19c68de
Shayaan Sadiq <shayaansadiq1402@gmail.com>
2026-04-08 17:02:34 +0530

    fix: Login error with invalid credentials

--- backend/src/main/java/com/agileinsight/backend/controller/OrganisationController.java
+++ backend/src/main/java/com/agileinsight/backend/controller/OrganisationController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.agileinsight.backend.ProjectResponse;
-import com.agileinsight.backend.model.Manager;
 import com.agileinsight.backend.model.Organisation;
 import com.agileinsight.backend.repository.OrganisationRepository;
 import com.agileinsight.backend.service.OrganisationService;
