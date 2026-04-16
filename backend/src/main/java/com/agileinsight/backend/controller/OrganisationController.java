b8b43fed72ee45807ed19c061606b761c62e472d
Shayaan Sadiq <shayaansadiq1402@gmail.com>
2026-04-17 03:55:12 +0530

    Refactored Organisation Controller for JWT Overhaul

--- backend/src/main/java/com/agileinsight/backend/controller/OrganisationController.java
+++ backend/src/main/java/com/agileinsight/backend/controller/OrganisationController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.agileinsight.backend.config.CustomUserDetails;
 import com.agileinsight.backend.model.Manager;
 import com.agileinsight.backend.model.Organisation;
 import com.agileinsight.backend.model.projection.OrganisationProjectionView;
@@ -26,8 +29,6 @@ import com.agileinsight.backend.service.OrganisationService;
 import com.agileinsight.backend.service.ProjectService;
 import com.agileinsight.backend.utility.JwtUtil;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
@@ -57,13 +58,13 @@ public class OrganisationController {
 
         if(organisation1 != null) {
             String id = organisation1.getId();
-            String token = jwtUtil.generateToken(organisation1.getEmail(), id);
+            String token = jwtUtil.generateToken(organisation1.getEmail(), id, "organisation");
             
             ResponseCookie cookie = ResponseCookie.from("jwt", token)
                                     .httpOnly(true)
                                     .secure(false)
                                     .path("/")
-                                    .maxAge(1000 * 60 * 60)
+                                    .maxAge(60 * 60)
                                     .sameSite("Lax")
                                     .build();
             
@@ -103,48 +104,14 @@ public class OrganisationController {
         }
     }
 
-    @GetMapping("/verify/{organisationId}")
-    public ResponseEntity<?> verifyJwt(@PathVariable @Valid String organisationId, HttpServletRequest request) {
-        String token = null;
-
-        if (request.getCookies() != null) {
-            for (Cookie cookie : request.getCookies()) {
-                if ("jwt".equals(cookie.getName())) {
-                    token = cookie.getValue();
-                }
-            }
-        }
-
-        if (token == null) {
-            return ResponseEntity.ok(Map.of(
-                "message","Not logged in"
-            ));
-        }
-
-        try {
-            String username = jwtUtil.extractUsername(token);
-            String id = jwtUtil.extractId(token);
-
-            if(!organisationId.equals(id)) {
-                throw new Exception();
-            }
-            
-            if (jwtUtil.validateToken(token, username) && organisationRepository.findById(id) != null) {
-                return ResponseEntity.ok(Map.of(
-                    "message", "Login successful",
-                    "id", id
-                ));
-            }
-
-        } catch (Exception e) {
-            return ResponseEntity.ok(Map.of(
-                "message","Invalid token"
-            ));
-        }
-
+    // Decide between 401 error (current) or give 200 with invalid token message
+    @PreAuthorize("hasRole('ORGANISATION')")
+    @GetMapping("/verify")
+    public ResponseEntity<?> verifyJwt(@AuthenticationPrincipal CustomUserDetails user) {
         return ResponseEntity.ok(Map.of(
-                "message","Not logged in"
-            ));
+            "message", "Login successful",
+            "id", user.getId()
+        ));
     }
     
     @GetMapping("/logout")
@@ -161,39 +128,25 @@ public class OrganisationController {
                 .body(Map.of("message", "Logout successful"));
     }
 
+    // Remove path variable, and use user.getId() to retrieve projects
+    // Decide between 401 error (current) or give 200 with invalid token message
+    @PreAuthorize("hasRole('ORGANISATION')")
     @GetMapping("/projects/{organisationId}")
     public ResponseEntity<?> getProjects(
-            @PathVariable String organisationId,
-            HttpServletRequest request) {
+            @PathVariable String organisationId) {
 
         List<ProjectResponse> projects = projectService.getAllOrganisationProjects(organisationId);
 
         return ResponseEntity.ok(projects);
     }
 
+    // Decide between 401 error (current) or give 200 with invalid token message
+    @PreAuthorize("hasRole('ORGANISATION')")
     @GetMapping("/project/{projectId}")
-    public ResponseEntity<?> getProject(@PathVariable String projectId, HttpServletRequest request) {
-        String token = null;
-
-        if (request.getCookies() != null) {
-            for (Cookie cookie : request.getCookies()) {
-                if ("jwt".equals(cookie.getName())) {
-                    token = cookie.getValue();
-                }
-            }
-        }
-
-        if (token == null) {
-            return ResponseEntity.ok(Map.of(
-                "message","Not logged in"
-            ));
-        }
-
+    public ResponseEntity<?> getProject(@PathVariable String projectId, @AuthenticationPrincipal CustomUserDetails user) {
         ProjectResponse projectResponse = projectService.getProject(projectId);
 
-        String tokenId = jwtUtil.extractId(token);
-
-        if (!projectResponse.getOrganisationId().equals(tokenId)) {
+        if (!projectResponse.getOrganisationId().equals(user.getId())) {
             return ResponseEntity.ok(Map.of(
                 "message","Project not found"
             ));
@@ -202,46 +155,22 @@ public class OrganisationController {
         return ResponseEntity.ok(projectResponse);
     }
 
+    // Remove path variable, and use user.getId() to retrieve projects
+    // Decide between 401 error (current) or give 200 with invalid token message
+    @PreAuthorize("hasRole('ORGANISATION')")
     @GetMapping("/getallmanagers/{organisationId}")
-    public ResponseEntity<?> getAllManagers(@PathVariable @Valid String organisationId, HttpServletRequest request) {
-        String token = null;
-
-        if (request.getCookies() != null) {
-            for (Cookie cookie : request.getCookies()) {
-                if ("jwt".equals(cookie.getName())) {
-                    token = cookie.getValue();
-                }
-            }
-        }
-
-        if (token == null) {
-            return ResponseEntity.ok(Map.of(
-                "message","Not logged in"
-            ));
-        }
+    public ResponseEntity<?> getAllManagers(@PathVariable @Valid String organisationId) {
         
         ArrayList<Manager> managers = managerRepository.findByOrganisationId(organisationId);
 
         return ResponseEntity.ok(managers);
     }
 
+    // Remove path variable, and use user.getId() to retrieve projects
+    // Decide between 401 error (current) or give 200 with invalid token message
+    @PreAuthorize("hasRole('ORGANISATION')")
     @GetMapping("/profile/{organisationId}")
-    public ResponseEntity<?> getProfile(@PathVariable @Valid String organisationId, HttpServletRequest request) {
-        String token = null;
-
-        if (request.getCookies() != null) {
-            for (Cookie cookie : request.getCookies()) {
-                if ("jwt".equals(cookie.getName())) {
-                    token = cookie.getValue();
-                }
-            }
-        }
-
-        if (token == null) {
-            return ResponseEntity.ok(Map.of(
-                "message","Not logged in"
-            ));
-        }
+    public ResponseEntity<?> getProfile(@PathVariable @Valid String organisationId) {
 
         OrganisationProjectionView organisationProjectionView = organisationRepository.findProjectedById(organisationId).orElse(null);
 
