e84f37322da7166b7b1790c79817a57106fbdf65
Shayaan Sadiq <shayaansadiq1402@gmail.com>
2026-04-22 11:31:57 +0530

    added /getTask endpoint

--- backend/src/main/java/com/agileinsight/backend/controller/TaskController.java
+++ backend/src/main/java/com/agileinsight/backend/controller/TaskController.java
@@ -3,10 +3,13 @@ package com.agileinsight.backend.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +17,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.agileinsight.backend.config.CustomUserDetails;
 import com.agileinsight.backend.model.Task;
 import com.agileinsight.backend.model.dto.TaskUpdateDTO;
+import com.agileinsight.backend.repository.ManagerRepository;
 import com.agileinsight.backend.repository.TaskRepository;
 import com.agileinsight.backend.service.TaskService;
 
@@ -31,10 +36,13 @@ public class TaskController {
 
     @Autowired
     private TaskRepository taskRepository;
+
+    @Autowired
+    private ManagerRepository managerRepository;
     
     @PreAuthorize("hasRole('MANAGER')")
     @PostMapping("/create")
-    public ResponseEntity<?> createTask(@RequestBody @Valid Task task) {
+    public ResponseEntity<?> createTask(@RequestBody @Valid Task task, @AuthenticationPrincipal CustomUserDetails user) {
         Task createdTask = taskService.createTask(task);
 
         if(createdTask != null) {
@@ -78,5 +86,26 @@ public class TaskController {
                 "message","Task not updated"
             ));
         }
+    }
+
+    @PreAuthorize("hasAnyRole('MANAGER', 'ORGANISATION')")
+    @GetMapping("/getTask/{taskId}")
+    public ResponseEntity<?> getTask(@PathVariable @Valid String taskId, @AuthenticationPrincipal CustomUserDetails user) {
+        Task task = taskRepository.findById(taskId).orElse(null);
+
+        if(task == null) {
+            return ResponseEntity.ok(Map.of(
+                "message","Invalid taskId"
+            ));
+        }
+
+        boolean isManagerValid = taskRepository.findById(taskId).orElse(null).getProjectId().equals(user.getId());
+        boolean isOrganisationValid = taskRepository.findById(taskId).orElse(null).getProjectId().equals(managerRepository.findById(user.getId()).orElse(null).getOrganisationId());
+
+        if(!isManagerValid || !isOrganisationValid) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        return ResponseEntity.ok(task);
     }
 }
